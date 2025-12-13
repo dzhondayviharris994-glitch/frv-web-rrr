5df875b237b56fe66b849761e274a1d4b20e8a10
forverunu13 <dzhondayviharris994@gmail.com-glitch>
2025-12-13 11:38:49 +0700

    added new files

--- main.py
+++ main.py
@@ -5,3 +5,20 @@ app = FastAPI()
 @app.get("/")
 def index():
     return {"message": "Hello, FastAPI"}
+
+@app.get("/joke")
+def Joke():
+    return {"author": "Sergei",
+            "joke": "FKFKKFKFKFKF"}
+
+@app.get("/telegram")
+def tg():
+    return {"Telegram": "@Forverunu"}
+
+@app.get("/what")
+def what():
+    return {"message": "what?"}
+
+@app.get("/haha")
+def index():
+    return {"jokerge": "hehehe"}
